d2f4508f37a0256d49e329215d1bab111ddbcd8a
C.W. Betts <computers57@hotmail.com>
2020-05-21 15:20:25 -0600

    Remove now-unused function getAllTexturesOf.

--- Pfhorge Source/Shapes Decoder/extractbitmap.h
+++ Pfhorge Source/Shapes Decoder/extractbitmap.h
@@ -10,8 +10,6 @@ typedef NS_ERROR_ENUM(LEShapesImportErrorDomain, LEShapesImportErrors) {
 	LEShapesImportReturnedNilImageRep,
 };
 
-NSArray<NSImage*> * _Nullable getAllTexturesOf(int theCollection, int theColorTable, const char *theShapesPath) NS_SWIFT_NAME(getAllTextures(collection:colorTable:shapesPath:));
-
 NSArray<NSImage*> * _Nullable getAllTexturesOfWithError(int theCollection, int theColorTable, NSURL *theShapesPath, NSError *_Nullable __autoreleasing* _Nullable outError) NS_REFINED_FOR_SWIFT;
 
 NS_ASSUME_NONNULL_END
